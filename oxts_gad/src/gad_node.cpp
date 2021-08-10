3f1c052ea2680f214f987cdc9e25deba67ea1bbc
smccarthy_oxts <smccarthy@oxts.com>
2021-08-10 17:14:28 +0100

    First implementation of position and attitude from odometry

--- oxts_gad/src/gad_node.cpp
+++ oxts_gad/src/gad_node.cpp
@@ -1,3 +1,8 @@
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+// #include "tf2_kdl/tf2_kdl.h"
+
 #include "oxts_gad/gad_node.hpp"
 #include "oxts_gad/nav_const.hpp"
 
@@ -28,6 +33,53 @@ void GadNode::nav_sat_fix_callback(const sensor_msgs::msg::NavSatFix::SharedPtr 
 void GadNode::odometry_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
 {
   RCLCPP_INFO(this->get_logger(), "odom received");
+
+  // Pose 
+  /// Position
+  OxTS::GadPosition gp = OxTS::GadPosition(140);
+  gp.SetPosLocal(
+    msg->pose.pose.position.x,
+    msg->pose.pose.position.y,
+    msg->pose.pose.position.z
+  );
+  gp.SetPosLocalVar(
+    msg->pose.covariance[0],
+    msg->pose.covariance[7],
+    msg->pose.covariance[14]
+  );
+  gp.SetTimeVoid();
+  gp.SetAidingLeverArmFixed(this->lva[0], this->lva[1], this->lva[2]);
+  gp.SetAidingLeverArmVar(this->lva_var[0], this->lva_var[1], this->lva_var[2]);
+
+  /// Orientation
+  OxTS::GadAttitude ga = OxTS::GadAttitude(141);
+  auto q_att = tf2::Quaternion();
+  tf2::convert(msg->pose.pose.orientation, q_att);
+  auto m_att = tf2::Matrix3x3(q_att);
+  double r, p, y;
+  m_att.getRPY(r, p, y);
+  ga.SetAtt(
+    y * NAV_CONST::RADS2DEG,
+    p * NAV_CONST::RADS2DEG,
+    r * NAV_CONST::RADS2DEG
+  );
+
+  // Twist (velocity)
+  /// Linear
+  msg->twist.twist.linear.x;
+  msg->twist.twist.linear.y;
+  msg->twist.twist.linear.z;
+
+  /// Angular
+  msg->twist.twist.angular.x;
+  msg->twist.twist.angular.y;
+  msg->twist.twist.angular.z;
+
+
+  // Twist Covariance
+  msg->twist.covariance;
+
+
 }
 
 
